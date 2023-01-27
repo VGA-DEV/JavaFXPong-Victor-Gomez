22302b30ef3f87aef07c4f73640e806a7807f766
Victor <victormanuelgomezalvarez_7239@escuelaces.net>
2023-01-27 14:04:32 +0100

    Creacion Palas

--- src/main/java/com/example/di_3/HelloApplication.java
+++ src/main/java/com/example/di_3/HelloApplication.java
@@ -3,16 +3,37 @@ package com.example.di_3;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(
+                new Pane(
+                        new Rectangle(20,100) {{
+                            setLayoutX(10);
+                            setLayoutY(10);
+                            setFill(Color.rgb(0, 0, 0));
+                        }},
+                        new Rectangle(20,100) {{
+                            setLayoutX(10);
+                            setLayoutY(10);
+                            setFill(Color.rgb(0, 0, 0));
+                        }}
+                ) {{
+                    requestFocus();
+                }}
+
+                , 320, 240);
+        stage.setTitle("PongFX");
         stage.setScene(scene);
         stage.show();
     }
