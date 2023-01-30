e5a924a24926831b3fc1f33c294b428f5022cf29
Victor <victormanuelgomezalvarez_7239@escuelaces.net>
2023-01-30 10:24:32 +0100

    Movimiento de la pelota en desarrollo

--- src/main/java/com/example/di_3/HelloApplication.java
+++ src/main/java/com/example/di_3/HelloApplication.java
@@ -1,5 +1,7 @@
 package com.example.di_3;
 
+import javafx.animation.Animation;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
@@ -14,18 +16,18 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Random;
 
 public class HelloApplication extends Application {
+    Rectangle pala1 = new Rectangle(15, 80);
+    Rectangle pala2 = new Rectangle(15, 80);
+    Circle pelota = new Circle(10, 10, 10);
 
     @Override
     public void start(Stage stage) throws IOException {
         //Colocacion elementos responsive
-        Rectangle pala1 = new Rectangle(15, 80);
         pala1.setFill(Color.rgb(255, 255, 255));
-        Rectangle pala2 = new Rectangle(15, 80);
-        ;
         pala2.setFill(Color.rgb(255, 255, 255));
-        Circle pelota = new Circle(8, 8, 8);
         pelota.setFill(Color.rgb(255, 255, 255));
         Scene scene = new Scene(
                 new Pane(
@@ -48,21 +50,29 @@ public class HelloApplication extends Application {
 
                 }}, 320, 240) {{
             setOnKeyPressed(keyEvent -> {
-                if (keyEvent.getCode() == KeyCode.W) {
+                if (keyEvent.getCode() == KeyCode.W && pala1.getLayoutY()>10) {
                     pala1.setLayoutY(pala1.getLayoutY() - 5);
                 }
-                if (keyEvent.getCode() == KeyCode.S) {
+                if (keyEvent.getCode() == KeyCode.S && pala1.getHeight()<getHeight()-90) {
                     pala1.setLayoutY(pala1.getLayoutY() + 5);
                 }
-                if (keyEvent.getCode() == KeyCode.UP) {
+                if (keyEvent.getCode() == KeyCode.UP && pala2.getLayoutY()>10) {
                     pala2.setLayoutY(pala2.getLayoutY() - 5);
                 }
-                if (keyEvent.getCode() == KeyCode.DOWN) {
+                if (keyEvent.getCode() == KeyCode.DOWN && pala2.getLayoutY()<getHeight()-90) {
                     pala2.setLayoutY(pala2.getLayoutY() + 5);
                 }
-
-
             });
+            AnimationTimer moverPelota= new AnimationTimer() {
+                int[] direccion={-1,1};
+                Random ra= new Random();
+                int dir= ra.nextInt(2);
+                @Override
+                public void handle(long l) {
+                }
+            };
+            moverPelota.start();
+
         }};
 
         stage.setTitle("PongFX");
@@ -73,4 +83,5 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
+
 }
