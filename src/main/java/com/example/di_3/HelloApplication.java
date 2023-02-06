019878e289dc9a8e354845d1611e660025d83942
Victor <victormanuelgomezalavarez_7239@escuelaces.net>
2023-02-06 10:45:29 +0100

    Dos palas a la vez y movimiento de pelota terminar

--- src/main/java/com/example/di_3/HelloApplication.java
+++ src/main/java/com/example/di_3/HelloApplication.java
@@ -2,6 +2,8 @@ package com.example.di_3;
 
 import javafx.animation.Animation;
 import javafx.animation.AnimationTimer;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
@@ -14,6 +16,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.util.Random;
@@ -22,6 +25,11 @@ public class HelloApplication extends Application {
     Rectangle pala1 = new Rectangle(15, 80);
     Rectangle pala2 = new Rectangle(15, 80);
     Circle pelota = new Circle(10, 10, 10);
+
+    private boolean abajoDer, abajoIzq, arribaDer, arribaIzq;
+
+    private int move = 5;
+
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -50,28 +58,48 @@ public class HelloApplication extends Application {
 
                 }}, 320, 240) {{
             setOnKeyPressed(keyEvent -> {
-                if (keyEvent.getCode() == KeyCode.W && pala1.getLayoutY()>10) {
-                    pala1.setLayoutY(pala1.getLayoutY() - 5);
+                if (keyEvent.getCode() == KeyCode.W && pala1.getLayoutY() > 10) {
+                    arribaIzq = true;
                 }
-                if (keyEvent.getCode() == KeyCode.S && pala1.getHeight()<getHeight()-90) {
-                    pala1.setLayoutY(pala1.getLayoutY() + 5);
+                if (keyEvent.getCode() == KeyCode.S && pala1.getHeight() < getHeight() - 90) {
+                    abajoIzq = true;
                 }
-                if (keyEvent.getCode() == KeyCode.UP && pala2.getLayoutY()>10) {
-                    pala2.setLayoutY(pala2.getLayoutY() - 5);
+                if (keyEvent.getCode() == KeyCode.UP && pala2.getLayoutY() > 10) {
+                    arribaDer = true;
                 }
-                if (keyEvent.getCode() == KeyCode.DOWN && pala2.getLayoutY()<getHeight()-90) {
-                    pala2.setLayoutY(pala2.getLayoutY() + 5);
+                if (keyEvent.getCode() == KeyCode.DOWN && pala2.getLayoutY() < getHeight() - 90) {
+                    abajoDer = true;
                 }
             });
-            AnimationTimer moverPelota= new AnimationTimer() {
-                int[] direccion={-1,1};
-                Random ra= new Random();
-                int dir= ra.nextInt(2);
-                @Override
-                public void handle(long l) {
+            setOnKeyReleased(keyEvent -> {
+                if (keyEvent.getCode() == KeyCode.W && pala1.getLayoutY() > 10) {
+                    arribaIzq = false;
                 }
-            };
-            moverPelota.start();
+                if (keyEvent.getCode() == KeyCode.S && pala1.getHeight() < getHeight() - 90) {
+                    abajoIzq = false;
+                }
+                if (keyEvent.getCode() == KeyCode.UP && pala2.getLayoutY() > 10) {
+                    arribaDer = false;
+                }
+                if (keyEvent.getCode() == KeyCode.DOWN && pala2.getLayoutY() < getHeight() - 90) {
+                    abajoDer = false;
+                }
+            });
+            Timeline timeline = new Timeline(new KeyFrame(Duration.millis(50), actionEvent -> {
+                pala1.setY(arribaIzq ? pala1.getY() : pala1.getY() + 10);
+                pala1.setY(abajoIzq ? pala1.getY() : pala1.getY() - 10);
+                pala2.setY(arribaDer ? pala2.getY() : pala2.getY() + 10);
+                pala2.setY(abajoDer ? pala2.getY() : pala2.getY() - 10);
+                if (pelota.getLayoutX() + move > getHeight() || pelota.getCenterX() - move < 0) {
+                    move*=-1;
+                }
+                pelota.setLayoutX(pelota.getLayoutY() + move);
+                pelota.setLayoutY(pelota.getLayoutX() + 5);
+            }
+            ));
+            timeline.setCycleCount(Animation.INDEFINITE);
+            timeline.play();
+
 
         }};
 
